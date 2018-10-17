2df0daae94502210da8fff5f4097bb605313b946
Nathan Miranda <nate845@gmail.com>
2018-10-17 00:28:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ main.remove()
 
 /* Create your new element here and assign it to newHeader */
 const header = document.createElement('h1')
-console.log(header)
+header.id = 'victory'
 console.log(header)
 const newHeader = null;
 
