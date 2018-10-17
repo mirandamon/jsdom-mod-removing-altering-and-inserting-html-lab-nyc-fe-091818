2a4b7d40cb02c25c59f47a96c86665754f67af5c
Nathan Miranda <nate845@gmail.com>
2018-10-17 00:26:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
-document.querySelector('#main').remove()
-
+//document.querySelector('#main').remove()
+main.remove()
 
 /* Create your new element here and assign it to newHeader */
+
 const newHeader = null;
+
