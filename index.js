d45a448c9ef13b383e71a3dbc52565fa6262bbef
Nathan Miranda <nate845@gmail.com>
2018-10-17 00:31:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const header = document.createElement('h1')
 header.id = 'victory'
 header.innerText = 'Nathan is the champion!'
 document.body.appendChild(header)
-const newHeader = null;
+const newHeader = document.querySelector('#victory');
 
